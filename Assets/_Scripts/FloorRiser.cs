f7012403ccb64b56524764174fcb7929891e56ae
Aleksandar Golubovic <golubovic.aleksandar01@gmail.com>
2023-03-28 15:26:32 +0200

    Added FloorRiser
    Use the animation curve to determine the speed of the floor

---
+++ Assets/_Scripts/FloorRiser.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorRiser : MonoBehaviour
+{
+    [SerializeField]
+    private AnimationCurve _testing;
+
+    private void Update()
+    {
+        Debug.Log("value: " + _testing.Evaluate(Time.fixedTime).ToString("F3") + " time: " + Time.fixedTime);
+
+        RaiseFloor();
+    }
+
+    private void Start()
+    {
+        Debug.Log(_testing.Evaluate(2f));
+    }
+
+    private void RaiseFloor()
+    {
+        var newPos = transform.position;
+        newPos.y += _testing.Evaluate(Time.fixedTime);
+        transform.position = newPos;
+    }
+}
